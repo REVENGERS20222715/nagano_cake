c7ab901b088890d5fe6ac2413e4af070eeda325e
gyooooo <harapeko2925@gmail.com>
2022-07-18 05:24:57 +0000

    gemfile install

--- Gemfile
+++ Gemfile
@@ -56,3 +56,8 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise'
+
+gem "enum_help"
+
+gem 'kaminari','~> 1.2.1'
+# ページネーション
--- test/models/order_detail_test.rb
+++ test/models/order_detail_test.rb
@@ -4,4 +4,6 @@ class OrderDetailTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  enum making_status: {cannot: 0, awaiting: 1, in_production: 2, complete: 3}
+  
 end
