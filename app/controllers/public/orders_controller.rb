84c45c55f29f8b776707c51f18b092136b8a81be
gyooooo <harapeko2925@gmail.com>
2022-07-19 04:31:48 +0000

    共通部分実装

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,8 +1,19 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order = Order.new
+  end
+  
+  def confirm
   end
 
   def complete
+  end
+  
+  def create
+    @order = Order.find(params[:id])
+    @order.customer_id = current_customer.id
+    @order.save
+    render 'orders/confirm'
   end
 
   def index
