886c7311d86c01ff870f3c8b6ca9f5c351fae20b
sunflower-field <himartmusume@gmail.com>
2022-07-19 09:48:38 +0000

    Adminのtopページ途中

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,5 +1,6 @@
 class OrderDetail < ApplicationRecord
-    
+
+    belongs_to :order
     enum making_status: {cannot: 0, awaiting: 1, in_production: 2, complete: 3}
-    
+
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "aa1c753b3d61471db24b8183e73ded11.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  config.hosts << "cc935b48bb5d44b687c5032558692add.vfs.cloud9.us-east-1.amazonaws.com"
 end
