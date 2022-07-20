8106203f67119ae43168d4e7181e1faad84fe503
sunflower-field <himartmusume@gmail.com>
2022-07-20 10:32:23 +0000

    [Add]admin/orders show&コントローラー

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,21 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @order = Order.find(params[:id])
+    @customer = @order.admin
   end
+
+  def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    @order_details = @order.order_details
+
+    if @order.status == "入金確認"
+      @order_details.each do |order_detail|
+        order_detail.make_status = "製作待ち"
+        order_detail.save
+      end
+    end
+    redirect_to admin_order_path
+  end
+
 end
