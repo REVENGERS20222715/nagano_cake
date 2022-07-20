e494234e271a74e528913e7096af066bcb43dd11
EC2 Default User <ec2-user@ip-172-31-5-55.ap-northeast-1.compute.internal>
2022-07-20 13:25:44 +0000

    ビュー作成途中

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,10 @@ class Item < ApplicationRecord
     # has_many :customers, through: :cart_items, dependent: :destroy, source: :customer
     has_many :cart_items, dependent: :destroy
     has_many :order_details
-
-
+    
+    validates :name, presence: true
+    validates :introduction, presence: true
+    validates :is_active, presence: true
+    validates :price, presence: true
+    validates :genre_id, presence: true
 end
