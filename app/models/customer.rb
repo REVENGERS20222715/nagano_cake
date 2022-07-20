b3f1faf49f3e057c5d8d7563f5d63bb3c24cb5a4
gyooooo <harapeko2925@gmail.com>
2022-07-20 10:33:17 +0000

    public customer途中

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,8 +5,39 @@ class Customer < ApplicationRecord
   # has_many :items, through: :cart_items, dependent: :destroy, source: :item
   has_many :cart_items, dependent: :destroy
   has_many :orders
+  
+  # enum is_active: {Available: true, Invalid: false}
+  #   def active_for_authentication?
+  #       super && (self.is_active === "Available")
+  #   end
+  # admin/customers/index
 
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  validates :first_name,presence:true
+  validates :last_name,presence:true
+  validates :first_name_kana,presence:true
+  validates :last_name_kana,presence:true
+  validates :post_address,presence:true,length: { is: 7 }
+
+  def full_address
+    '〒'+postal_code+' '+address
+  end
+
+  def full_name
+    first_name+' '+last_name
+  end
+
+  def self.search(search,word)
+    if search == "forward"
+      @customers = Customer.where("first_name LIKE?","#{word}%")
+    elsif search =="partial"
+      @customers = Customer.where("first_name LIKE?","%#{word}%")
+    else
+      @customers = Customer.all
+    end
+  end
+
 end
