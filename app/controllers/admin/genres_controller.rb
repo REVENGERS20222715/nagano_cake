2daf3b8bbacb0d75d799b31bf6557c1b979399fb
EC2 Default User <ec2-user@ip-172-31-79-189.ec2.internal>
2022-07-20 09:54:47 +0000

    ジャンル機能

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,8 +1,10 @@
 class Admin::GenresController < ApplicationController
+  # before_action :authenticate_admin!,only: [:create,:edit,:update,:index]
 
   def new
   @item = current_customer.items.build
   @genres = Genre.all
+  @genre = Genre.new
   end
 
   def index
@@ -11,16 +13,30 @@ class Admin::GenresController < ApplicationController
   end
 
   def create
-    @genre = Genre.new(book_params)
-    if
-      @genre.save
-      redirect_to admin_genres_path
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      redirect_to admin_genres_path(@genre)
     else
+      @genres = Genre.all
       render 'index'
     end
   end
 
   def edit
     @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+      redirect_to admin_genres_path
+    else
+      render "edit"
+    end
+  end
+
+  private
+  def genre_params
+    params.require(:genre).permit(:name)
   end
 end
