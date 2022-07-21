4eacd51bdc452a91eea4af1eafda645bba1bdea2
EC2 Default User <ec2-user@ip-172-31-79-189.ec2.internal>
2022-07-21 12:19:13 +0000

    customer途中

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
 
     has_one_attached :image
-    belongs_to :genres
+    belongs_to :genre
     # has_many :customers, through: :cart_items, dependent: :destroy, source: :customer
     has_many :cart_items, dependent: :destroy
     has_many :order_details
