d62a3eccb3ba46742dcf4e2ee16a9eae421dfdf9
sunflower-field <himartmusume@gmail.com>
2022-07-26 06:28:58 +0000

    インデント修正

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,17 +1,8 @@
 class Customer < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   has_many :addresses
-  # has_many :items, through: :cart_items, dependent: :destroy, source: :item
   has_many :cart_items, dependent: :destroy
   has_many :orders
-
-  # enum is_active: {Available: true, Invalid: false}
-  #   def active_for_authentication?
-  #       super && (self.is_active === "Available")
-  #   end
-  # admin/customers/index
-
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
@@ -21,20 +12,11 @@ class Customer < ApplicationRecord
   validates :first_name_kana,presence:true
   validates :last_name_kana,presence:true
   validates :postal_code,presence:true,length: { is: 7 }
-  # ちょうど７桁じゃないとバリデーションがかかって保存されない
+  # ７桁のバリデーションがかかって保存されない
 
   def full_address
     '〒'+postal_code+' '+address
   end
-
-
-
-  # def customer_status
-  # end
-
-  # def full_name
-  #   first_name+' '+last_name
-  # end
 
   def self.search(search,word)
     if search == "forward"
